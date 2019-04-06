965710f39cb25a71599ffe553b9101dcc7fabb3d
atheimuz <atheimuz@naver.com>
2019-04-06 23:35:35 +0900

    add scss file, navigation component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -14,12 +13,4 @@ export default {
 <style lang="scss">
 
 @import 'src/assets/css/source/style.scss';
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ frontend/src/components/Navigation.vue
@@ -0,0 +1,34 @@
+<template>
+<nav>
+  <ul>
+    <li class="logo">
+      <a href="" class="icon-sprite instagram-logo_ver1"><span>logo</span></a>
+      <div class="bar"><span>bar</span></div>
+      <a href="" class="icon-sprite instagram-logo_ver2"><span>logo</span></a>
+    </li>
+    <li class="search">
+      <input type="text" placeholder="검색"/>
+    </li>
+    <li class="others">
+      <a href="" class="icon-sprite explore"><span>explore</span></a>
+      <div class="icon-sprite heart"><span>heart</span></div>
+      <a href="" class="icon-sprite user"><span>user</span></a>
+    </li>
+  </ul>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
