1310c9e8ecf1aa529dae417565543e8c09735746
atheimuz <atheimuz@naver.com>
2019-04-07 21:44:18 +0900

    add sprite image, navigation design

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -2,17 +2,19 @@
 <nav>
   <ul>
     <li class="logo">
-      <a href="" class="icon-sprite instagram-logo_ver1"><span>logo</span></a>
+      <a href="" class="icon-sprite ico-glyph instagram-logo_ver1"><span>logo</span></a>
       <div class="bar"><span>bar</span></div>
-      <a href="" class="icon-sprite instagram-logo_ver2"><span>logo</span></a>
+      <a href="" class="icon-sprite ico-glyph instagram-logo_ver2"><span>logo</span></a>
     </li>
     <li class="search">
       <input type="text" placeholder="검색"/>
+      <div class="searchIcon ico-core icon-sprite"><span>search</span></div>
+      <div class="searchClear ico-core icon-sprite"><span>search clear</span></div>
     </li>
     <li class="others">
-      <a href="" class="icon-sprite explore"><span>explore</span></a>
-      <div class="icon-sprite heart"><span>heart</span></div>
-      <a href="" class="icon-sprite user"><span>user</span></a>
+      <a href="" class="icon-sprite ico-glyph explore"><span>explore</span></a>
+      <div class="icon-sprite ico-glyph heart"><span>heart</span></div>
+      <a href="" class="icon-sprite ico-glyph user"><span>user</span></a>
     </li>
   </ul>
 </nav>
