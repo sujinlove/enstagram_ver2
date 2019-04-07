d46a04836d59033b8060fe5515ea503f44c7a672
atheimuz <atheimuz@naver.com>
2019-04-08 01:30:08 +0900

    vue build in spring boot path

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -45,12 +45,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-//    index: path.resolve(__dirname, '../../src/main/resources/static/index.html'),
-	index: path.resolve(__dirname, '../../index.html'),
+    index: path.resolve(__dirname, '../../src/main/resources/static/index.html'),
+//	index: path.resolve(__dirname, '../../index.html'),
 
     // Paths
-//    assetsRoot: path.resolve(__dirname, '../../src/main/resources/static'),
-  	assetsRoot: path.resolve(__dirname, '../../'),
+    assetsRoot: path.resolve(__dirname, '../../src/main/resources/static'),
+//  	assetsRoot: path.resolve(__dirname, '../../'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
