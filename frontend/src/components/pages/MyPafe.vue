07e5d52c0ac8fd9466c9154767ad4fbc291ce258
atheimuz <atheimuz@naver.com>
2019-04-12 23:56:23 +0900

    add mypage

---
+++ frontend/src/components/pages/MyPafe.vue
@@ -0,0 +1,17 @@
+<template>
+  <one-column></one-column>
+</template>
+
+<script>
+import OneColumn from './common/OneColumn'
+
+export default {
+  components: {
+    OneColumn
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+</style>
