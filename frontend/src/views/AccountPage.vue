63b418f476d87e8563773dbf32187d19bb8b1380
dlgodls <dlgodls530@naver.com>
2019-04-16 12:03:04 +0900

    change column in Account Page

--- frontend/src/views/AccountPage.vue
+++ frontend/src/views/AccountPage.vue
@@ -1,22 +1,22 @@
 <template>
   <section>
-    <two-columns>
-      <div slot="main">
+    <one-column>
+      <div>
         <img src="https://www.instagram.com/static/images/homepage/home-phones.png/43cc71bb1b43.png" alt="" />
       </div>
-      <account slot="sidebar"></account>
-    </two-columns>
+      <account></account>
+    </one-column>
   </section>
 </template>
 
 <script>
-import TwoColumns from '../components/common/TwoColumns'
+import OneColumn from '../components/common/OneColumn'
 import Feed from '../components/Feed'
 import Account from '../components/Account'
 
 export default {
   components: {
-    TwoColumns,
+    OneColumn,
     Feed,
     Account
   }
