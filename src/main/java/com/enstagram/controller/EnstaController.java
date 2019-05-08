d3382667fa55d5d803f5f0fd9db8b867d05e2135
dlgodls <dlgodls530@naver.com>
2019-05-08 19:10:46 +0900

    Add create function

--- src/main/java/com/enstagram/controller/EnstaController.java
+++ src/main/java/com/enstagram/controller/EnstaController.java
@@ -1,7 +1,6 @@
 package com.enstagram.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +13,7 @@ public class EnstaController {
 	/*
 	 * Get board_account Table DB
 	 */
+	
 	@Autowired EnstaService enstaService;
 
 	@RequestMapping("/list")
@@ -24,6 +24,7 @@ public class EnstaController {
 	/*
 	 * Create Account
 	 */
+	
 	@RequestMapping(value="/account", method=RequestMethod.POST)
 	public String create(EnstaAccount enstaAccount) {
 		return enstaService.create(enstaAccount);
