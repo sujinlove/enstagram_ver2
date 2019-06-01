1aeb30cd4c29773cae9b37d43914e7fcca1ea091
atheimuz <atheimuz@naver.com>
2019-06-01 13:04:17 +0900

    Change proxy in Vue

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -14,6 +14,14 @@ module.exports = {
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true
+      },
+      '/upload': {
+        target: 'http://localhost:8080/',
+        changeOrigin: true
+      },
+      '/profile': {
+        target: 'http://localhost:8080/',
+        changeOrigin: true
       }
     },
 
