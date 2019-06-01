1a0dae1521f857c82208ba8a43b03997360fcf15
atheimuz <atheimuz@naver.com>
2019-06-02 03:39:45 +0900

    Add comment to feed component

--- frontend/src/components/Feed.vue
+++ frontend/src/components/Feed.vue
@@ -1,5 +1,6 @@
 <template>
 <article class="feed">
+  <!--When Mouse Over in My Page-->
   <div class="feed-hover" v-if="page == 'MyPage'">
     <div v-if="this.feed.heart > 0">
       <button class="icon-sprite ico-core heart"><span>heart</span></button>
@@ -10,6 +11,7 @@
       <span>0</span>
     </div>
   </div>
+  <!--When Mouse Over in My Page End-->
   <header class="feed-header">
     <div class="user-pic">
       <router-link :to= "{ name: 'UserPage', params: { user_id: this.user.id }}">
@@ -26,7 +28,9 @@
       <img :src="this.feed.file_name" alt="feed" onerror="this.style.display='none'"/>
     </div>
   </div>
+  <!--When Not Feed Comment-->
   <div class="feed-content" v-if="this.$store.state.editFeed === false">
+    <!--Feed Comment in Feed Page-->
     <div class="feed-comments feed-content-inner" v-if="page === 'FeedPage'">
       <ul>
         <li class="feed-comment" v-if="this.feed.description !== ''">
@@ -53,6 +57,8 @@
         </li>
       </ul>
     </div>
+    <!--Feed Comment in Feed Page End-->
+    <!--Feed Content in All Page-->
     <div class="feed-content-inner">
       <div class="feed-btn">
         <button class="icon-sprite ico-glyph-3 heart" @click="addHeart"  v-if="this.$store.state.user.heartList.indexOf(this.feed_num) === -1"><span>heart</span></button>
@@ -69,34 +75,40 @@
       <div class="heart-count" v-else>
         가장 먼저 <span @click="addHeart">좋아요</span>를 눌러보세요
       </div>
+      <!--Feed Content in Main Page, My Page-->
       <div class="content-view" v-if="page !== 'FeedPage'">
         <ul>
-          <li class="feed-comment">
+          <li class="feed-comment" v-if="this.feed.description !== ''">
             <router-link :to= "{ name: 'UserPage', params: { user_id: this.user.id }}" class="user-id">{{this.user.id}}</router-link>
             <span class="feed-text">{{this.feed.description}}</span>
           </li>
-          <li class="more-comment" v-if="page == 'MainPage'">
+          <li class="more-comment">
             <router-link to= "/feed">
                 <span>댓글</span>
                 <span>1231231</span>
                 <span>개 모두 보기</span>
             </router-link>
           </li>
-          <li class="user-comment" v-if="page == 'MainPage'">
+          <li class="user-comment">
             <router-link to= "" class="user-id">jennierubyjane</router-link>
             <span class="user-text">나는 메인 제니!</span>
           </li>
         </ul>
       </div>
+      <!--Feed Content in Main Page, My Page End-->
     </div>
+    <!--Feed Content in All Page End-->
     <div class="comment">
       <textarea placeholder="댓글 달기..."/>
       <button type="button">게시</button>
     </div>
   </div>
+  <!--When Not Feed Comment End-->
+  <!--When Edit Feed Comment-->
   <div class="feed-content edit-comment" v-else>
     <textarea placeholder="문구 입력..." v-model="feed.description"/>
   </div>
+  <!--When Edit Feed Comment End-->
 </article>
 </template>
 
