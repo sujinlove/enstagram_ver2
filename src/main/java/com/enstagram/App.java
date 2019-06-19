b720669339196a0fa5ac7dd3455eb034e27e0bc2
atheimuz <atheimuz@naver.com>
2019-06-19 18:08:18 +0900

    Fix timezone

--- src/main/java/com/enstagram/App.java
+++ src/main/java/com/enstagram/App.java
@@ -1,19 +1,10 @@
 package com.enstagram;
 
-import java.util.TimeZone;
-
-import javax.annotation.PostConstruct;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-    @PostConstruct
-	void started() {
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-	}
-    
     public static void main(String[] args) {
         SpringApplication.run(App.class, args); // (5)
         System.out.println("Hello Enstagram");
