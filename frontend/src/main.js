5409cac8557d822eb883bc7c5813f714e5b1c7e5
dlgodls <dlgodls530@naver.com>
2019-06-21 18:57:58 +0900

    Add es6-promise config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'es6-promise/auto'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import store from './store'
